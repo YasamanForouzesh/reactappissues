d662f701b046276ca4b3076a77e6bf71f2370208
Ilka Santiago <home@Ilkas-MacBook-Pro.local>
2021-02-11 13:27:46 -0800

    Header.js was updated

--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -1,15 +1,22 @@
 import { Link } from 'react-router-dom'
-
+import {   useState} from "react";
 export default function Header() {
+    const [filter,setFilter]=useState("")
+    const handlerFilter=(status)=>{
+        setFilter(status)
+        console.log(status)
+    }
     return(
-        <header>
-                <img style={{width: 100}} src="https://cdn3.iconfinder.com/data/icons/social-media-logos-flat-colorful-1/2048/5347_-_Github-512.png" />
-            <nav>
-                Pull Requests{' | '}
-                <Link to='/issues'>Issues</Link>{' | '}
-                Marketplace{' | '}
-                Explore
+        <header className="header">
+                <img className="gitImag"  src="https://cdn3.iconfinder.com/data/icons/social-media-logos-flat-colorful-1/2048/5347_-_Github-512.png" />
+            <nav className="navBar">
+                <button className="bar">Pull Requests</button> 
+                <button className="bar">Pull Requests</button> 
+                <button className={`bar ${filter==="issues" ? "isActive" : ""}`} onClick={()=>handlerFilter("issues")}><Link className="link" to='/issues'>Issues</Link></button> 
+                <button className={`bar ${filter==="Marketplace" ? "isActive" : ""}`} onClick={()=>handlerFilter("Marketplace")}>Marketplace</button>
+                <button className="bar">Explore</button>
+                
             </nav>
         </header>
     )
-}
+
