72c8f1faf60840d467dad5aa2aeff2e79a4983af
Drake Tyksinski <drake.tyksinski@live.com>
2021-02-11 10:29:46 -0800

    added navbar

---
+++ src/components/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return(
+        <div>
+            <h1>Looking to submit an issue?</h1>
+            <p>Please read the posting guidelines before opening an issue</p>
+        </div>
+    )
+}
